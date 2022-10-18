070f2d6b0f1ec0e45493216d3a3893a172af0e89
Amarajah <naomi.ibeh69@gmail.com>
2022-10-18 11:45:00 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -12,6 +12,7 @@ int _printf(const char *format, ...)
 	int sum = 0;
 	va_list ap;
 	char *p, *start;
+	params_t params = PARAMS_INIT;
 
 	va_start(ap, format);
 
@@ -19,4 +20,32 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+
+		if (*p != '%')/*checking for the % specifier*/
+		{
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
+		{
+			p++; /* next char */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+					params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
